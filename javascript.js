7dba659167284a41f8437291a4daeaa074af5cfc
Aska-codeprogramer <askaalghifari487@gmail.com>
2025-07-05 12:53:44 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+function click1() {
+    alert ("mau liat instagram ku.....");
+    console.log ("click instagram");
+}
+function click2() {
+    alert ("mau liat tiktok ku..... ");
+    console.log ("click tiktok");
+}
+function roodmap() {
+    alert ("roodmap front end devaloper....");
+    console.log ("click front end devaloper");
+}
+function sertifikat1() {
+    window.open ('sertifikat1.pdf', '_blank');
+}
+function sertifikat2() {
+    window.open ('sertifikat2.pdf', '_blank');
+}
+function sertifikat3() {
+    window.open ('sertifikat3.pdf', '_blank');
+}
+function sertifikat4() {
+    window.open ('sertifikat4.pdf', '_blank');
+}
+function sertifikat5() {
+    window.open ('sertifikat5.pdf', '_blank');
+}
+function sertifikat6() {
+    window.open ('sertifikat6.pdf', '_blank');
+}
